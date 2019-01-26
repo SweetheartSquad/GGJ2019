9321f73b5153534b2afe82eb738e1ea99f90db77
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-01-26 17:17:08 -0500

    wave update

--- src/assets/textures/textures.js
+++ src/assets/textures/textures.js
@@ -2,5 +2,7 @@ export { default as fella } from './fella.png';
 export { default as fella_run_1 } from './fella1.png';
 export { default as fella_run_2 } from './fella2.png';
 export { default as shadows } from './shadows.png';
-export { default as wave } from './wave.png';
+export { default as wave1 } from './wave1.png';
+export { default as wave2 } from './wave2.png';
+export { default as wave3 } from './wave3.png';
 export { default as boat } from './boat.png';
