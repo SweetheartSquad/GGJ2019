5c4937d7e0e595ec6c973274f467d38508982772
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-01-26 15:51:15 -0500

    roughing out collision stuff

---
+++ src/Bounds.js
@@ -0,0 +1,52 @@
+import { Polygon } from "pixi.js/lib/core/math";
+import Point from "pixi.js/lib/core/math/Point";
+import V from './vector';
+import {
+	checkIntersection,
+  } from 'line-intersect';
+
+export class Bounds extends Polygon {
+	constructor(points = []) {
+		super(points);
+		this.close();
+		this.center = new Point(0, 0);
+		points.forEach(({ x, y }) => {
+			this.center.x += x;
+			this.center.y += y;
+		});
+		this.center.x /= points.length;
+		this.center.y /= points.length;
+	}
+
+	dist(p,r) {
+		let minDist = Infinity;
+		let result;
+		for(let i = 0; i < this.points.length-1; i += 2) {
+			const {
+				[i]: x1,
+				[i+1]: y1,
+				[i+2]: x2,
+				[i+3]: y2,
+			} = this.points;
+			const { point } = checkIntersection(p.x, p.y, r.x, r.y, x1, y1, x2, y2);
+			if (!point) { continue; }
+			const dist = V.magnitude2(V.subtract(p, point));
+			if (dist < minDist) {
+				minDist = dist;
+				result = point;
+			}
+		}
+		return result;
+	}
+
+	onEnter() {
+
+	}
+
+	onExit() {
+
+	}
+}
+
+
+export default Bounds;
--- src/TestScene.js
+++ src/TestScene.js
@@ -1,9 +1,20 @@
 import { getInput, setScene } from './main';
 import game, { resources } from './Game';
-import { Container, Sprite } from 'pixi.js/lib/core';
+import { Container, Sprite, Point } from 'pixi.js';
 import PoseScene from './PoseScene';
 import Character from './Character';
+import Bounds from './Bounds';
+import V from './vector';
+import { Graphics } from 'pixi.js/lib/core';
 let player;
+let bounds = new Bounds([
+	new Point(140 + Math.random() * 40,140 + Math.random() * 40),
+	new Point(400 + Math.random() * 40,140 + Math.random() * 40),
+	new Point(400 + Math.random() * 40,400 + Math.random() * 40),
+	new Point(140 + Math.random() * 40,400 + Math.random() * 40),
+]);
+let g = new Graphics();
+console.log(bounds);
 export default class TestScene extends Container {
 	constructor() {
 		super();
@@ -12,8 +23,11 @@ export default class TestScene extends Container {
 		player.camPoint = new PIXI.DisplayObject();
 		player.camPoint.visible = false;
 		player.con.addChild(player.camPoint);
+		player.p.x = bounds.center.x;
+		player.p.y = bounds.center.y;
 		// characters.characters.push(player);
 
+		this.addChild(g);
 		this.addChild(player.con);
 
 		const font = {
@@ -33,6 +47,7 @@ export default class TestScene extends Container {
 		player.con.addChild(text);
 	}
 	update() {
+		g.clear();
 		const curTime = game.app.ticker.lastTime;
 		
 		const input = getInput();
@@ -43,19 +58,24 @@ export default class TestScene extends Container {
 		player.v.y *= 0.8;
 		player.v.y += input.move.y;
 
-		if (player.p.y > 70) {
-			player.v.y -= (player.p.y - 70) / 3;
+		const start = player.p;
+		const end = bounds.center;
+		const d = bounds.dist(start, end);
+		g.beginFill();
+		g.lineStyle(3, 400000);
+		g.moveTo(start.x, start.y);
+		g.lineTo(end.x, end.y);
+		g.endFill();
+		if (d) {
+			g.beginFill();
+			g.lineStyle(3, 300000);
+			g.moveTo(player.p.x, player.p.y);
+			g.lineTo(d.x, d.y);
+			g.endFill();
+			const v = V.subtract(d, player.p);
+			player.v.x += v.x/5;
+			player.v.y += v.y/5;
 		}
-		if (player.p.y < -70) {
-			player.v.y -= (player.p.y + 70) / 3;
-		}
-
-		// if (player.p.x > WORLD_RIGHT) {
-		// 	player.v.x -= (player.p.x - WORLD_RIGHT) / 3;
-		// }
-		// if (player.p.x < WORLD_LEFT) {
-		// 	player.v.x -= (player.p.x - WORLD_LEFT) / 3;
-		// }
 
 		player.p.x += player.v.x;
 		var oldy = player.p.y;
@@ -86,6 +106,10 @@ export default class TestScene extends Container {
 		}
 
 		player.update();
+		g.beginFill(0,0);
+		g.lineStyle(3, 200000);
+		g.drawPolygon(bounds);
+		g.endFill();
 		// if (this.ready && getInput().justDown) {
 		// 	this.destroy();
 		// 	setScene(new PoseScene());
