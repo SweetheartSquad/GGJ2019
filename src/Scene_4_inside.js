720e7ccfc48fe081e4a8093e686b589ddf06fa49
Ryan Bluth <ryanbluth93@gmail.com>
2019-01-27 16:05:50 -0500

    Fixed scene 4

--- src/Scene_4_inside.js
+++ src/Scene_4_inside.js
@@ -1,6 +1,7 @@
 import InteriorScene from './InteriorScene';
 import { toExterior, toNextDay } from './helpers';
 import Scene_4_outside from './Scene_4_outside';
+import Scene_5_outside from './Scene_5_outside';
 
 export default class extends InteriorScene {
 	constructor() {
