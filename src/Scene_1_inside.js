d35db92bb184200da36b9b550f8175d1070c2c93
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-01-27 15:48:18 -0500

    Update Scene_1_inside.js

--- src/Scene_1_inside.js
+++ src/Scene_1_inside.js
@@ -3,16 +3,34 @@ import Scene_1_outside from './Scene_1_outside';
 import { toExterior, toNextDay } from './helpers';
 import Scene_2_outside from './Scene_2_outside';
 
-export default class PlayScene3 extends InteriorScene {
+export default class extends InteriorScene {
 	constructor() {
 		super({
 			npcs: [{
+				name: 'editor',
+				x: -230,
+				y: 250,
+				lines: [
+					'This storm is really bad...',
+					'I don’t... remember anything.',
+					'Are we going to be okay?',
+				],
+			}, {
+				name: 'whizkid',
+				x: -130,
+				y: 250,
+				lines: [
+					'Man, what\'s going on?',
+					'I just want to go home and see my mom.',
+				],
+			}, {
 				name: 'dame',
-				scale: 0.2,
 				x: 130,
 				y: 250,
-				label: 'say hi',
-				lines: ['hello', 'whats up', 'im done talking now'],
+				lines: [
+					'I haven\'t seen a storm like this in many years.',
+					'Be careful out there.',
+				],
 			}],
 			interact: [
 				toExterior(Scene_1_outside),
