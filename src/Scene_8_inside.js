64c0f14cb5e2d98e02ec58e0b70d2117a35b4283
Ryan Bluth <ryanbluth93@gmail.com>
2019-01-27 16:09:12 -0500

    More fixxs

--- src/Scene_8_inside.js
+++ src/Scene_8_inside.js
@@ -1,6 +1,7 @@
 import InteriorScene from './InteriorScene';
 import { toExterior, toNextDay } from './helpers';
 import Scene_8_outside from './Scene_8_outside';
+import Scene_9_outside from './Scene_9_outside';
 
 export default class extends InteriorScene {
 	constructor() {
