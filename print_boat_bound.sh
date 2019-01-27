11bbb8a7daf093592d6bb7ad74f17afa9054b759
Ryan Bluth <ryanbluth93@gmail.com>
2019-01-27 12:53:58 -0500

    print bounds

---
+++ print_boat_bound.sh
@@ -0,0 +1,9 @@
+echo "{"
+cat ./bounds/boat_scene/bound1.svg | python ./svg_path.py
+echo "},{"
+cat ./bounds/boat_scene/bound2.svg | python ./svg_path.py
+echo "},{"
+cat ./bounds/boat_scene/bound3.svg | python ./svg_path.py
+echo "},{"
+cat ./bounds/boat_scene/bound4.svg | python ./svg_path.py
+echo "}"
