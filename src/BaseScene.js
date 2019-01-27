85ea2e5c31bb1278b979652ce521c217c32f0e06
Sean S. LeBlanc <sean.leblanc@plays.tv>
2019-01-27 02:21:10 -0500

    separate generic update

--- src/BaseScene.js
+++ src/BaseScene.js
@@ -2,6 +2,9 @@ import { Container, Point, Graphics } from "pixi.js/lib/core";
 import NavMesh from "./NavMesh";
 import InteractableMesh from "./InteractableMesh";
 import { createDialog } from "./Dialog";
+import { player } from "./main";
+import { lerp } from "./utils";
+import size from "./size";
 
 let g = new Graphics();
 
@@ -45,6 +48,27 @@ export default class BaseScene extends Container{
 		floor.addChild(g);
 		floor.addChild(player);
 		this.addChild(floor);
+	}
+
+	update(){
+		this.bounds.update(player);
+		this.interactiveBounds.update(player);
+
+		if (Math.abs(player.v.y) > 0.01) {
+			this.floor.sortDirty = true;
+		}
+
+		// camera
+		this.s = lerp(this.s || 1, 1 - (Math.abs(player.v.y)+Math.abs(player.v.x))/64, 0.05);
+		this.scale.x = this.scale.y = lerp(this.scale.x, this.s, 0.2);
+	
+		var p = this.toLocal(PIXI.zero, player.camPoint);
+		this.x = lerp(this.x, p.x, 0.1);
+		this.y = lerp(this.y, player.p.y*this.scale.y, 0.1);
+		this.pivot.x = Math.floor(this.x);
+		this.pivot.y = Math.floor(this.y);
+		this.x = size.x/2;
+		this.y = size.y/4*1;
 	}
 
 	debugDraw(){
--- src/BoatScene.js
+++ src/BoatScene.js
@@ -105,31 +105,13 @@ export default class BoatScene extends BaseScene{
 	}
 
 	update() {
+		super.update();
 		const curTime = game.app.ticker.lastTime;
 		turbulenceTarget = window.turbulence || turbulenceTarget;
 		turbulence = lerp(turbulence, turbulenceTarget, 0.1);
 		highSeas = lerp(0.3, 4, turbulence);
 		this.screenFilter.uniforms.curTime = curTime;
 		this.screenFilter.uniforms.rain = turbulence;
-
-		this.bounds.update(player);
-		this.interactiveBounds.update(player);
-
-		if (Math.abs(player.v.y) > 0.01) {
-			this.floor.sortDirty = true;
-		}
-
-		// camera
-		this.s = lerp(this.s || 1, 1 - (Math.abs(player.v.y)+Math.abs(player.v.x))/64, 0.05);
-		this.scale.x = this.scale.y = lerp(this.scale.x, this.s, 0.2);
-	
-		var p = this.toLocal(PIXI.zero, player.camPoint);
-		this.x = lerp(this.x, p.x, 0.1);
-		this.y = lerp(this.y, player.p.y*this.scale.y, 0.1);
-		this.pivot.x = Math.floor(this.x);
-		this.pivot.y = Math.floor(this.y);
-		this.x = size.x/2;
-		this.y = size.y/4*1;
 
 		this.floor.rotation = ((Math.sin(curTime / 300) + Math.sin(curTime / 200)) * 0.5 * 0.01) * highSeas;
 		const waveY = 0.5 * (Math.sin(curTime / 300) + Math.sin(curTime / 400)) + Math.sin(curTime / 10) * 0.05 * Math.sin(curTime / 50);
